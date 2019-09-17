900bedbdb9e4f0d8b8b8c42a02e0245cd77b731c
Sodjiro <kadyr91@yahoo.com>
2019-09-17 22:11:15 +0300

    finl

--- quiz1.R
+++ quiz1.R
@@ -11,14 +11,25 @@ download.file(fileurlcodeBook,'codeBook.pdf',mode = "wb")
 valmil<-subset(filedata,VAL>23)
 propmil<-length(valmil$VAL)
 
+##excel file
 fileUrlxlsx<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
 download.file(fileUrlxlsx,"NaturalGas.xlsx",mode = "wb")
 wb <- loadWorkbook("NaturalGas.xlsx")
-dat <- readWorksheet(wb, sheet = 1, startRow = 17, startCol = 7,
-                     endRow = 23, endCol = 15,header = TRUE,forceConversion = TRUE)
+dat <- readWorksheet(wb, sheet = 1, startRow = 17, startCol = 7,endRow = 23, endCol = 15,header = TRUE,forceConversion = TRUE)
 names(dat)<-dat[1,]
 dat<-dat[-1,]
 dat$Zip<-as.numeric(dat$Zip)
 dat$Ext<-as.numeric(dat$Ext)
 sum(dat$Zip*dat$Ext,na.rm=T)
+
+##xml File
+doc<-xmlTreeParse("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml",useInternalNodes = TRUE)
+zipcodes<-xpathSApply(doc,"//zipcode",xmlValue)
+zipcodes<-zipcodes[zipcodes==21231]
+nzip21231<-length(zipcodes)
+
+##DataTable
+fileUrlcsv2<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrlcsv2,'housingIdaho2.csv')
+DT<-fread("./housingIdaho2.csv")
 
