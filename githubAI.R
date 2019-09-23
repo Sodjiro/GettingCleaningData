618243ca429c965ae6ba1e86e8b75958cf60d051
Sodjiro <kadyr91@yahoo.com>
2019-09-24 01:03:10 +0300

    find https://github.com/jtleek/datasharing created time

---
+++ githubAI.R
@@ -0,0 +1,35 @@
+library(httr)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# 2. To make your own application, register at
+#    https://github.com/settings/developers. Use any URL for the homepage URL
+#    (http://github.com is fine) and  http://localhost:1410 as the callback url
+#
+#    Replace your key and secret below.
+myapp <- oauth_app("github",
+                   key = "67ef040a483dbefd5fd1",
+                   secret = "491e9088728bf96dbe3de8bbea6f5b14224a6596"
+)
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+stop_for_status(req)
+content(req)
+
+# OR:
+req <- with_config(gtoken, GET("https://api.github.com/users/jtleek/repos"))
+stop_for_status(req)
+content(req)
+
+
+# 1.
+for (i in 1:30) b[i]<-content2[[i]]$html_url
+# 2.
+res <- lapply(content2, function(ch) grep("https://github.com/jtleek/datasharing", ch))
